a46ee85ab1e0a2c3a3272a37e2bfee4a160a898f
psvetovas <psvetovas@yahoo.com>
2017-12-28 13:54:01 -0800

    Added possibility to create assert in print class

--- app/src/main/java/creatlab/dviratis/Logs.java
+++ app/src/main/java/creatlab/dviratis/Logs.java
@@ -31,5 +31,18 @@ public class Logs {
 
     }
 
+    public void Assert(String txt){
+
+        if (Logs) {
+
+            throw new AssertionError(txt);
+
+        }
+        else{
+            Log.i(ERROR_LOG_TAG, txt);
+        }
+
+    }
+
 
 }
