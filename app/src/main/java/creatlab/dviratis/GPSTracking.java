b29ffb2551c7142ff65290fcc2d909a106a4343a
psvetovas <psvetovas@yahoo.com>
2018-01-06 12:27:46 -0800

    Commited final project

--- app/src/main/java/creatlab/dviratis/GPSTracking.java
+++ app/src/main/java/creatlab/dviratis/GPSTracking.java
@@ -30,6 +30,8 @@ public class GPSTracking {
 
     public boolean checkLocationPermission(){
 
+        //test
+
         boolean permission = true;
 
         if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
