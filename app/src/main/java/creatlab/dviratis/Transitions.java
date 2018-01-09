d0616b292bf0c42210953b2904fb26e8554952ad
psvetovas <psvetovas@yahoo.com>
2018-01-09 12:48:16 -0800

    New transition class

---
+++ app/src/main/java/creatlab/dviratis/Transitions.java
@@ -0,0 +1,29 @@
+package creatlab.dviratis;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Juozas on 1/9/2018.
+ */
+
+public class Transitions {
+
+    Context TransitionContext;
+
+    public Transitions(Context Transitions) {
+
+        this.TransitionContext = Transitions;
+
+    }
+
+
+    public void GoToActivity(String CurrentActivity, String NewActivity) {
+
+
+
+    }
+}
